d7e5a24a5d6de81bebc4752a6845e0f9f3447898
gptoto <55799783+gptoto@users.noreply.github.com>
2019-11-05 18:53:53 +0100

    Add files via upload
    Ajout des fichiers

---
+++ precache-manifest.b3d6c688c1ef6f7b1365ef6cb4aaba89.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "d4b133d69f27bc5fe036",
+    "url": "/js/chunk-vendors.73b6a63d.js"
+  },
+  {
+    "revision": "6ce04ca7893e5e61555d",
+    "url": "/js/app.1885d59c.js"
+  },
+  {
+    "revision": "7622682a61756988a1cf9cb8eaf77ead",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79abb823a858a8ee6e25b3ae182a5fe8",
+    "url": "/img/logo+(62).79abb823.png"
+  },
+  {
+    "revision": "1fc042992992a66fb13c04e53781c672",
+    "url": "/img/754632.1fc04299.jpg"
+  },
+  {
+    "revision": "d4b133d69f27bc5fe036",
+    "url": "/css/chunk-vendors.33c19d1b.css"
+  },
+  {
+    "revision": "6ce04ca7893e5e61555d",
+    "url": "/css/app.00e4359e.css"
+  }
+];
